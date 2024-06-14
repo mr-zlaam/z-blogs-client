604ec2b60f73f38f569beffece14199c36a3f26a
Zlaam <mrzalaam@gmail.com>
2024-06-14 22:31:42 +0500

    first commit

---
+++ server.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.listen(8000, () => {
+  console.log(`server is running on ${8000}`);
+});
