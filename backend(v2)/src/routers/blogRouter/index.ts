c5e8544dede3cb8f65ec0487683ccffef0d4f385
Zlaam <mrzalaam@gmail.com>
2024-06-28 16:14:02 +0500

    "Added authentication middleware to blog router endpoints for create, update, delete, and get all private blogs"

--- backend(v2)/src/routers/blogRouter/index.ts
+++ backend(v2)/src/routers/blogRouter/index.ts
@@ -10,15 +10,29 @@ import {
 } from "../../controllers/blogController/blogController";
 import { validateData } from "../../middlewares/validationMiddleware";
 import { BlogValidation } from "../../schemas";
+import {
+  ifUserIsAdmin,
+  ifUserIsModerator_OR_Admin,
+} from "../../middlewares/authMiddleware";
 
 const blogRouter = Router();
 blogRouter
   .route("/createBlog")
-  .post(validateData(BlogValidation), createBlogController);
-blogRouter.route("/getAllBlogs").get(getAllBlogsController);
-blogRouter.route("/getAllPrivateBlogs").get(getAllPrivateBlogsController);
+  .post(
+    validateData(BlogValidation),
+    ifUserIsModerator_OR_Admin,
+    createBlogController
+  );
+blogRouter.route("/getAllPublicBlogs").get(getAllBlogsController);
+blogRouter
+  .route("/getAllPrivateBlogs")
+  .get(ifUserIsAdmin, getAllPrivateBlogsController);
 blogRouter.route("/getSingleBlog/:blogSlug").get(getSingleBlogController);
-blogRouter.route("/updateBlog/:blogSlug").put(updateBlogController);
-blogRouter.route("/deleteBlog/:blogSlug").delete(deleteBlogController);
+blogRouter
+  .route("/updateBlog/:blogSlug")
+  .put(ifUserIsAdmin, updateBlogController);
+blogRouter
+  .route("/deleteBlog/:blogSlug")
+  .delete(ifUserIsAdmin, deleteBlogController);
 blogRouter.route("/getAllBlogs/search").get(searchBlogController);
 export { blogRouter };
