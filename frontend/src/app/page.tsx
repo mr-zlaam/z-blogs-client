7c99cd1d3e667a4b52772f8a000f5c949c08bc75
zlaam <zlaam.dev@gmail.com>
2024-08-17 01:06:51 +0500

    update page.tsx and button.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import {} from "react";
-
 function App() {
   return (
     <>
-      <section>App</section>
+      <section>
+        <Button variant={"destructive"}>Hello</Button>
+      </section>
     </>
   );
 }
