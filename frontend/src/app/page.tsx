7b81af108c6c44322de654dac8762d5090235f41
zlaam <zlaam.dev@gmail.com>
2024-08-17 13:42:32 +0500

    update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { ThemeToggler } from "@/theme/ThemeToggler";
 import {} from "react";
 function App() {
   return (
     <>
-      <section>
-        <Button variant={"destructive"}>Hello</Button>
-        <ThemeToggler />
-      </section>
+      <section>Hello world</section>
     </>
   );
 }
