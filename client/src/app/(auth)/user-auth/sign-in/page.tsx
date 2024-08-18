bcec8250a26cf0862c7e81c11246f8732aa30ef9
zlaam <zlaam.dev@gmail.com>
2024-08-18 22:37:31 +0500

    update page.tsx

--- client/src/app/(auth)/user-auth/sign-in/page.tsx
+++ client/src/app/(auth)/user-auth/sign-in/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import ButtonLoader from "@/_subComponents/buttonLoader";
+import ButtonLoader from "@/_subComponents/buttonLoader/buttonLoader";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
