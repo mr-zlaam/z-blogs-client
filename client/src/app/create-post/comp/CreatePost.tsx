3c403aca5cd8c0a64196e869095424a2aea7ea65
zlaam <zlaam.dev@gmail.com>
2024-08-19 02:25:08 +0500

    add editor

--- client/src/app/create-post/comp/CreatePost.tsx
+++ client/src/app/create-post/comp/CreatePost.tsx
@@ -2,14 +2,27 @@
 import PageWrapper from "@/app/_components/pageWrapper/PageWrapper";
 import { Button } from "@/components/ui/button";
 import { Link } from "@/components/ui/link";
-import {} from "react";
+import { useState } from "react";
 import Editor from "./editor/Editor";
 
 function CreatePost({ token, uid }: { token: string; uid: string }) {
+  const [isPreviewOpen, setIsPreviewOpen] = useState(false);
   console.log(uid);
   return (
     <>
-      <Button className="fixed right-4 top-5">Preview</Button>
+      {isPreviewOpen && (
+        <div className="h-screen fixed top-0 left-0 w-full bg-background text-foreground z-[99]">
+          <PageWrapper>hello</PageWrapper>
+        </div>
+      )}
+      <Button
+        className="fixed right-4 top-5 z-[100]"
+        onClick={() => {
+          setIsPreviewOpen((prev) => !prev);
+        }}
+      >
+        {isPreviewOpen ? "Hide Preview" : "ShowPreview"}
+      </Button>
       <Link
         varient="expand-from-left"
         href={"/home"}
