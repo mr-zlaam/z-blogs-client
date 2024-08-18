0184d94cccad2ec6147c3361d8b498a4418fe03a
zlaam <zlaam.dev@gmail.com>
2024-08-18 22:51:20 +0500

    feat: create CreatePost.tsx and page.tsx

---
+++ client/src/app/create-post/comp/CreatePost.tsx
@@ -0,0 +1,11 @@
+import {} from "react";
+
+function CreatePost() {
+  return (
+    <>
+      <section>CreatePost</section>
+    </>
+  );
+}
+
+export default CreatePost;
---
+++ client/src/app/create-post/page.tsx
@@ -0,0 +1,11 @@
+import {} from "react";
+
+function CreatePostPage() {
+  return (
+    <>
+      <section>CreatePostPage</section>
+    </>
+  );
+}
+
+export default CreatePostPage;
