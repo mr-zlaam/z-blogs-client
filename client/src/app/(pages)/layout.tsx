99a11272aad8387a7dcee59e7e310cf368b3e019
zlaam <zlaam.dev@gmail.com>
2024-08-18 14:42:03 +0500

    update layout.tsx and NavLinks.ts

--- client/src/app/(pages)/layout.tsx
+++ client/src/app/(pages)/layout.tsx
@@ -9,9 +9,9 @@ function PageLayout({ children }: { children: React.ReactNode }) {
     <>
       <PageWrapper className="sticky top-0 left-0 w-full bg-background mb-4">
         <Header />
-        <Separator className="bg-black/10 h-[0.5px]" />
+        <Separator className="bg-foreground/10 h-[0.5px]" />
         <PageHeader />
-        <Separator className="bg-black/10 h-[0.5px]" />
+        <Separator className="bg-foreground/10 h-[0.5px]" />
       </PageWrapper>
       <PageWrapper className="py-3">{children}</PageWrapper>
     </>
