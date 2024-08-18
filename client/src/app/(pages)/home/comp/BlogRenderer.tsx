1fac1386d87a479903de513724aa7e60d06d1a05
zlaam <zlaam.dev@gmail.com>
2024-08-18 23:40:31 +0500

    update BlogRenderer.tsx

--- client/src/app/(pages)/home/comp/BlogRenderer.tsx
+++ client/src/app/(pages)/home/comp/BlogRenderer.tsx
@@ -8,8 +8,15 @@ interface blogPostProps {
 function BlogRenderer({ blogPosts }: blogPostProps) {
   console.log(blogPosts, "from blog renderer");
   return (
-    <section className="px-2">
-      <div className="border-solid border border-foreground md:h-[350px] h-[300px] rounded-md"></div>
+    <section className="px-2 ">
+      <div className="border-solid border border-foreground/20 my-5 shadow-lg shadow-foreground/50 md:h-[350px] h-[300px] rounded-md"></div>
+      <div className="border-solid border border-foreground/20 my-5 shadow-lg shadow-foreground/50 md:h-[350px] h-[300px] rounded-md"></div>
+      <div className="border-solid border border-foreground/20 my-5 shadow-lg shadow-foreground/50 md:h-[350px] h-[300px] rounded-md"></div>
+      <div className="border-solid border border-foreground/20 my-5 shadow-lg shadow-foreground/50 md:h-[350px] h-[300px] rounded-md"></div>
+      <div className="border-solid border border-foreground/20 my-5 shadow-lg shadow-foreground/50 md:h-[350px] h-[300px] rounded-md"></div>
+      <div className="border-solid border border-foreground/20 my-5 shadow-lg shadow-foreground/50 md:h-[350px] h-[300px] rounded-md"></div>
+      <div className="border-solid border border-foreground/20 my-5 shadow-lg shadow-foreground/50 md:h-[350px] h-[300px] rounded-md"></div>
+      <div className="border-solid border border-foreground/20 my-5 shadow-lg shadow-foreground/50 md:h-[350px] h-[300px] rounded-md"></div>
     </section>
   );
 }
