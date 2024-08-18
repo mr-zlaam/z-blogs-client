92fccc51a57f58e375567b21903aad18c1e1ef44
zlaam <zlaam.dev@gmail.com>
2024-08-18 14:00:50 +0500

    "Rename PageHeader component to PascalCase and remove file"

--- client/src/app/(pages)/layout.tsx
+++ client/src/app/(pages)/layout.tsx
@@ -2,13 +2,16 @@ import {} from "react";
 import PageWrapper from "../_components/pageWrapper/PageWrapper";
 import Header from "../_components/header/Header";
 import { Separator } from "@radix-ui/react-separator";
+import PageHeader from "../_components/pageHeader/pageHeader";
 
 function PageLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <PageWrapper className="sticky top-0 left-0 w-full bg-background mb-4">
         <Header />
-        <Separator />
+        <Separator className="bg-black/10 h-[0.5px]" />
+        <PageHeader />
+        <Separator className="bg-black/10 h-[0.5px]" />
       </PageWrapper>
       <PageWrapper className="py-3">{children}</PageWrapper>
     </>
---
+++ client/src/app/_components/pageHeader/pageHeader.tsx
@@ -0,0 +1,11 @@
+import {} from "react";
+
+function PageHeader() {
+  return (
+    <>
+      <section>PageHeader</section>
+    </>
+  );
+}
+
+export default PageHeader;
