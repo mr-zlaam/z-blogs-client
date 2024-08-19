97f9e34b45ae6ef6e5c661a2bc47fc173b93b233
zlaam <zlaam.dev@gmail.com>
2024-08-19 22:37:20 +0500

    Remove useInView hook and related code from BlurImage component.

--- client/src/app/(pages)/home/comp/BlurImage.tsx
+++ client/src/app/(pages)/home/comp/BlurImage.tsx
@@ -1,27 +1,21 @@
 "use client";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
-import { useInView } from "react-intersection-observer";
 interface ImageProps {
   src: string;
   alt: string;
   base64: string;
 }
 function BlurImage({ src, alt, base64 }: ImageProps) {
-  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.5 });
   return (
     <>
       <Image
-        ref={ref}
         src={src}
         alt={alt}
         fill
         placeholder="blur"
         blurDataURL={base64}
-        className={cn(
-          "w-auto transition-opacity duration-400 ease-in-out object-contain",
-          inView ? "opacity-100" : "opacity-0"
-        )}
+        className={cn("w-auto")}
       />
     </>
   );
