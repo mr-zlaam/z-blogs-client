42e5f76a6429e21c2854e16f32e9adbec52a58f2
zlaam <zlaam.dev@gmail.com>
2024-08-19 23:39:20 +0500

    update BlogRenderer.tsx and BlurImage.tsx

--- client/src/app/(pages)/home/comp/BlogRenderer.tsx
+++ client/src/app/(pages)/home/comp/BlogRenderer.tsx
@@ -11,10 +11,10 @@ interface blogPostProps {
 }
 
 async function BlogRenderer({ post }: blogPostProps) {
-  const buffer = await fetch(post?.blogThumbnail).then(async (res) =>
-    Buffer.from(await res.arrayBuffer())
-  );
-  const { base64 } = await getPlaiceholder(buffer);
+  // const buffer = await fetch(post?.blogThumbnail).then(async (res) =>
+  //   Buffer.from(await res.arrayBuffer())
+  // );
+  // const { base64 } = await getPlaiceholder(buffer);
   return (
     <section className="px-2 ">
       <div className="relative  md:h-fit py-3 h-[300px] rounded-md flex justify-start  items-start  overflow-hidden ">
@@ -22,7 +22,7 @@ async function BlogRenderer({ post }: blogPostProps) {
           <BlurImage
             src={post.blogThumbnail}
             alt={post.blogTitle}
-            base64={base64}
+            // base64={base64}
             width={500}
             height={200}
             className="rounded"
@@ -38,6 +38,9 @@ async function BlogRenderer({ post }: blogPostProps) {
             Published : {moment(post.createdAt).format("MMMM Do, YYYY")}
           </span>
           <BlogDescription blogDescription={post.blogDescription as string} />
+          <Link href={``} className="my-2 block w-fit mx-auto">
+            Read More...
+          </Link>
         </div>
       </div>
       <Separator />
