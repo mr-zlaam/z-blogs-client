346cf9619ef0a19dd1bafcc3b5f194e904cc9294
zlaam <zlaam.dev@gmail.com>
2024-08-19 23:40:43 +0500

    update BlogRenderer.tsx

--- client/src/app/(pages)/home/comp/BlogRenderer.tsx
+++ client/src/app/(pages)/home/comp/BlogRenderer.tsx
@@ -1,20 +1,19 @@
 import { Link } from "@/components/ui/link";
 import { Separator } from "@/components/ui/separator";
 import { BlogDataTypes } from "@/types";
-import { marked } from "marked";
-import { getPlaiceholder } from "plaiceholder";
-import BlurImage from "./BlurImage";
 import moment from "moment";
+import { getPlaiceholder } from "plaiceholder";
 import BlogDescription from "./BlogDescription";
+import BlurImage from "./BlurImage";
 interface blogPostProps {
   post: BlogDataTypes;
 }
 
 async function BlogRenderer({ post }: blogPostProps) {
-  // const buffer = await fetch(post?.blogThumbnail).then(async (res) =>
-  //   Buffer.from(await res.arrayBuffer())
-  // );
-  // const { base64 } = await getPlaiceholder(buffer);
+  const buffer = await fetch(post?.blogThumbnail).then(async (res) =>
+    Buffer.from(await res.arrayBuffer())
+  );
+  const { base64 } = await getPlaiceholder(buffer);
   return (
     <section className="px-2 ">
       <div className="relative  md:h-fit py-3 h-[300px] rounded-md flex justify-start  items-start  overflow-hidden ">
@@ -22,7 +21,7 @@ async function BlogRenderer({ post }: blogPostProps) {
           <BlurImage
             src={post.blogThumbnail}
             alt={post.blogTitle}
-            // base64={base64}
+            base64={base64}
             width={500}
             height={200}
             className="rounded"
