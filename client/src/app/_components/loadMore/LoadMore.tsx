6c2bc515acc0a8a84f27c8ba820ff28059e02117
zlaam <zlaam.dev@gmail.com>
2024-08-23 22:30:56 +0500

    feat: create PageLoader.tsx and LoadMore.tsx

---
+++ client/src/app/_components/loadMore/LoadMore.tsx
@@ -0,0 +1,12 @@
+import PageLoader from "@/_subComponents/pageLoader/PageLoader";
+import {} from "react";
+
+function LoadMore() {
+  return (
+    <>
+      <PageLoader />
+    </>
+  );
+}
+
+export default LoadMore;
