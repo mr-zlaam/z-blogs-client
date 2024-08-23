e2bb607a57fd15a612536959d04480266a620579
zlaam <zlaam.dev@gmail.com>
2024-08-23 23:23:44 +0500

    "Added useState and ref to LoadMore component, imported useState and BlogDataTypes, and created an empty fetchBlogs function."

--- client/src/app/_components/loadMore/LoadMore.tsx
+++ client/src/app/_components/loadMore/LoadMore.tsx
@@ -2,12 +2,18 @@
 import { useInView } from "react-intersection-observer";
 
 import PageLoader from "@/_subComponents/pageLoader/PageLoader";
-
+import { useState } from "react";
+import { BlogDataTypes } from "@/types";
+const fetchBlogs = async () => {};
 function LoadMore() {
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+  const [blogs, setBlogs] = useState<BlogDataTypes[]>([]);
   return (
-    <>
+    <div ref={ref}>
       <PageLoader />
-    </>
+    </div>
   );
 }
 
