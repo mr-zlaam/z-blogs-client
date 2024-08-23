09ad222c0f4064b38acfe1eeae992019241c7f8c
zlaam <zlaam.dev@gmail.com>
2024-08-23 17:46:06 +0500

    update SendOTP.tsx and alert-dialog.tsx

--- client/src/app/_components/send-otp/SendOTP.tsx
+++ client/src/app/_components/send-otp/SendOTP.tsx
@@ -1,10 +1,44 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import {} from "react";
 
 function SendOTP() {
+  const handleSendOTP = async () => {};
   return (
-    <span className="flex items-center text-red-500 text-sm cursor-pointer">
-      Verify Account
-    </span>
+    <>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <span className="flex items-center text-red-500 text-sm cursor-pointer">
+            Verify Account
+          </span>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Do you want to verify your account?
+            </AlertDialogTitle>
+            <AlertDialogDescription className="text-sm font-normal ">
+              You'll recieve 6 digit otp on your email you used for registration
+              which will expire in 30 minutes please verify your account using
+              that otp
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Yes</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 }
 
