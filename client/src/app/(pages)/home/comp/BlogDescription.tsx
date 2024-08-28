491742281cfbb47e2b0b113765ef1f8e266350bd
zlaam <zlaam.dev@gmail.com>
2024-08-28 20:04:49 +0500

    update BlogDescription.tsx

--- client/src/app/(pages)/home/comp/BlogDescription.tsx
+++ client/src/app/(pages)/home/comp/BlogDescription.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Link } from "@/components/ui/link";
 import { SITE_VERSION } from "@/constants";
 import { useRouter } from "next/navigation";
 
@@ -10,21 +11,14 @@ function BlogDescription({
   blogDescription: string;
   blogSlug: string;
 }) {
-  const router = useRouter();
-  const rawHtml = blogDescription;
   return (
     <>
-      <span
-        onClick={() => {
-          blogSlug &&
-            router.push(`/${SITE_VERSION}/${blogSlug}
-`);
-        }}
-        dangerouslySetInnerHTML={{
-          __html: rawHtml,
-        }}
+      <Link
         className="text-foreground/70 my-2  text-sm font-normal cursor-pointer select-none "
-      ></span>{" "}
+        href={`/${SITE_VERSION}/${blogSlug}`}
+      >
+        {blogDescription}
+      </Link>
     </>
   );
 }
