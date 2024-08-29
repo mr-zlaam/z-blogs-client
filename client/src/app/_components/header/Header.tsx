1295c3d3d73e5d9f8b5142fc253fe4db723a7da6
zlaam <zlaam.dev@gmail.com>
2024-08-29 17:15:58 +0500

    refactor the code and removed unused imports

--- client/src/app/_components/header/Header.tsx
+++ client/src/app/_components/header/Header.tsx
@@ -6,7 +6,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
@@ -17,14 +16,13 @@ import { useMessage } from "@/hooks/useMessage";
 import logo from "@/images/logo/z-logo.png";
 import { PayLoadType } from "@/types";
 import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { FaUserCircle } from "react-icons/fa";
 import { RandomAvatar } from "react-random-avatars";
 import SendOTP from "../send-otp/SendOTP";
 
 function Header({ user, token }: { user: PayLoadType; token: string }) {
   const { successMessage } = useMessage();
-  const router = useRouter();
   const pathName = usePathname();
   if (pathName === "/all-posts") return null;
   const logoutTheUser = async () => {
@@ -170,106 +168,6 @@ function Header({ user, token }: { user: PayLoadType; token: string }) {
                   )}
                 </DropdownMenuContent>
               </DropdownMenu>
-              {/* <Popover>
-                <PopoverTrigger className="bg-transparent cursor-pointer  border-none flex items-center justify-center">
-                  {user && user.uid ? (
-                    <RandomAvatar
-                      size={35}
-                      mode="random"
-                      name={(user && user.username) || "hero"}
-                      square
-                    />
-                  ) : (
-                    <FaUserCircle size={35} />
-                  )}
-                </PopoverTrigger>
-                <PopoverContent className="h-fit w-[200px] shadow-md shadow-foreground/20">
-                  {user && user.fullName && (
-                    <div className="flex-[1] p-3">
-                      <p className="flex flex-col p-2">
-                        <span className="font-normal cursor-pointer">
-                          {user.fullName}
-                        </span>
-                        <span className="text-sm hover:underline text-foreground/70 cursor-pointer">
-                          @{user.username}
-                        </span>
-                      </p>
-                    </div>
-                  )}
-                  <Separator />
-
-                  <div className="flex-[2] flex flex-col  ">
-                    {user && user.role === "ADMIN" && (
-                      <Link
-                        href={"/create-post"}
-                        className="p-2 my-1 text-foreground hover:bg-foreground/10 duration-200 transition-all cursor-pointer font-normal rounded"
-                      >
-                        Create Post
-                      </Link>
-                    )}
-                    {user && user.role === "MODERATOR" && (
-                      <Link
-                        href={"/create-post"}
-                        className="p-2 my-1 text-foreground hover:bg-foreground/10 duration-200 transition-all cursor-pointer font-normal rounded"
-                      >
-                        Create Post
-                      </Link>
-                    )}
-
-                    {user && user.role === "ADMIN" && (
-                      <Link
-                        href={"/admin/users"}
-                        className="p-2 my-1 text-foreground hover:bg-foreground/10 duration-200 transition-all cursor-pointer font-normal rounded"
-                      >
-                        Dashboard
-                      </Link>
-                    )}
-                    {user && (
-                      <span className="p-2 my-1 text-foreground hover:bg-foreground/10 duration-200 transition-all cursor-pointer font-normal rounded">
-                        {user && user.isVerfied ? (
-                          <span className="flex items-center ">
-                            Verified
-                            <IoIosCheckmark
-                              size={25}
-                              className="text-foreground mx-4"
-                            />
-                          </span>
-                        ) : (
-                          <SendOTP
-                            email={user && (user.email as string)}
-                            token={token as string}
-                          />
-                        )}
-                      </span>
-                    )}
-                    <Link
-                      href={"/settings"}
-                      className="p-2 my-1 text-foreground hover:bg-foreground/10 duration-200 transition-all cursor-pointer font-normal rounded"
-                    >
-                      Settings
-                    </Link>
-                  </div>
-                  <Separator />
-
-                  <div>
-                    {user ? (
-                      <span
-                        onClick={logoutTheUser}
-                        className="text-red-500 select-none flex-1 block p-2 my-1 hover:bg-foreground/10 duration-200 transition-all cursor-pointer font-normal rounded"
-                      >
-                        Sign Out
-                      </span>
-                    ) : (
-                      <Link
-                        href={"/user-auth/sign-in"}
-                        className="text-green-600 select-none flex-1 block p-2 my-1  hover:bg-foreground/10 duration-200 transition-all cursor-pointer  rounded"
-                      >
-                        Sign in
-                      </Link>
-                    )}
-                  </div>
-                </PopoverContent>
-              </Popover> */}
             </div>
           </div>
         </nav>
