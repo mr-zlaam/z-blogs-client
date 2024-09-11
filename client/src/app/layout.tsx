f6eff8b889ba7cd30ea9f39cfb52f8309364873a
mr-zlaam <zlaam.dev@gmail.com>
2024-09-12 00:47:09 +0500

    "Updated metadata and added Head import in layout.tsx"

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -6,16 +6,28 @@ import { Arimo } from "next/font/google";
 import NextTopLoader from "nextjs-toploader";
 import "./editor.global.css";
 import "./globals.css";
+import Head from "next/head";
 const arimo = Arimo({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
 });
 export const metadata: Metadata = {
+  metadataBase: new URL("https://zlaam-vercel.app/"),
   title: {
-    default: "zlaam.dev",
-    template: "%s  zlaam.dev",
+    default: "Zlaam - Read the quality articles",
+    template: "%s - Zlaam - Read the quality articles",
   },
-  description: "The habit of reading is awesome.",
+  description:
+    "The collection of wonderful bussiness articles for developers and tech enthusiasts!",
+  openGraph: {
+    title: "Zlaam | Read the quality articles",
+    description:
+      "The collection of wonderful bussiness articles for developers and tech enthusiasts!",
+    type: "website",
+    locale: "en_US",
+    url: "https://zlaam-vercel.app/",
+    siteName: "Zlaam",
+  },
 };
 
 export default function RootLayout({
