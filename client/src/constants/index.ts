a27a8a4500d6d069ed81b68c45393752ab1fe435
mr-zlaam <zlaam.dev@gmail.com>
2024-09-12 13:42:42 +0500

    "Update REVALIDATE constant from 28800 to 300"

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -3,6 +3,6 @@ export const SITE_VERSION = 1;
 export const DELAY = 500;
 export const LIMIT = 20;
 export const PAGE = 2;
-export const REVALIDATE = 28800; // experiment
+export const REVALIDATE = 300; //28800 = 8hours
 export const SITE_URL = "https://zlaam.vercel.app";
 export const IS_NOT_DEV_ENV = true; // check if it is in development environment
