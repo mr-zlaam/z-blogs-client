e5aefcffdbeb5e24dbd3eda38af096a061dff926
mr-zlaam <zlaam.dev@gmail.com>
2024-09-13 22:17:49 +0500

    Remove unnecessary comment in fetchSearchPublicBlogs function.

--- client/src/helper/fetch/fetchData.ts
+++ client/src/helper/fetch/fetchData.ts
@@ -55,7 +55,6 @@ export const fetchSearchPublicBlogs = async (
   page: number,
   limit = LIMIT
 ) => {
-  // helo testing
   try {
     const response = await axios.get(
       `${BACKEND_URI}/blog/getAllBlogs/search?q=${query}&limit=${limit}&page=${page}`,
