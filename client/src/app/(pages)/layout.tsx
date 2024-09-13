9ea7ce567eb427eb1379481703a676d8f7289847
mr-zlaam <zlaam.dev@gmail.com>
2024-09-14 01:44:10 +0500

    "Removed axios import alias, moved getUserInformationFromToken function definition, updated fetchSingleUser function to use BACKEND_URI, and changed PageLayout function from async to sync"

--- client/src/app/(pages)/layout.tsx
+++ client/src/app/(pages)/layout.tsx
@@ -7,41 +7,44 @@ import {} from "react";
 import Header from "../_components/header/Header";
 import PageHeader from "../_components/pageHeader/PageHeader";
 import PageWrapper from "../_components/pageWrapper/PageWrapper";
-import { axios } from "@/axios";
-// ** fetch from the token const getUserInformationFromToken = () => {
-//   const objToken = UseCookieGrabber();
-//   if (!objToken?.value) {
-//     return;
-//   }
-//   try {
-//     const user = verify(objToken.value, SECRET) as PayLoadType;
-//     return user;
-//   } catch (error: any) {
-//     console.log(error.message);
-//   }
-// };
+import axios from "axios";
+const getUserInformationFromToken = () => {
+  const objToken = UseCookieGrabber();
+  if (!objToken?.value) {
+    return;
+  }
+  try {
+    const user = verify(objToken.value, SECRET) as PayLoadType;
+    return user;
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
 const fetchSingleUser = async (token: string) => {
   try {
     if (!token) return;
     const user = verify(token, SECRET) as PayLoadType;
-    const response = await axios.get(`/auth/getSingleUser/${user.uid}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const response = await axios.get(
+      `${BACKEND_URI}/auth/getSingleUser/${user.uid}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
     return response.data;
   } catch (error: any) {
     console.log(error);
   }
 };
-async function PageLayout({ children }: { children: React.ReactNode }) {
+function PageLayout({ children }: { children: React.ReactNode }) {
   const tokenStore = UseCookieGrabber();
-  const userfromdb = await fetchSingleUser(tokenStore?.value || "");
+  const user = getUserInformationFromToken() as PayLoadType;
   return (
     <>
       {/* <Alert /> */}
       <PageWrapper className="sticky top-0 left-0 w-full  mb-4 bg-background/80 backdrop-blur-md z-[50]">
-        <Header user={userfromdb.data} token={tokenStore?.value as string} />
+        <Header user={user} token={tokenStore?.value as string} />
         <Separator className="bg-foreground/10 h-[0.5px]" />
         <PageHeader />
         <Separator className="bg-foreground/10 h-[0.5px]" />
