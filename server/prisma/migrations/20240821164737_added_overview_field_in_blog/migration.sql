bf157a3ca913bf454ed296a9fea67cbb35723366
mr-zlaam <zlaam.dev@gmail.com>
2024-09-22 16:03:06 +0500

    delete 58 files

--- server/prisma/migrations/20240821164737_added_overview_field_in_blog/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "BlogPost" ADD COLUMN     "blogOverView" TEXT;
-
--- CreateIndex
-CREATE INDEX "BlogPost_blogTitle_idx" ON "BlogPost"("blogTitle");
--- server/server.ts
+++
@@ -1,6 +0,0 @@
-import connectDB from "./src/db";
-connectDB()
-  .then(() => console.log("success:true"))
-  .catch((err) => {
-    console.log("success:false");
-  });
