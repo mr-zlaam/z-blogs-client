6d2435b3aadc6bc9820579e27e4dc84add189d7e
mr-zlaam <zlaam.dev@gmail.com>
2024-10-24 20:22:14 +0500

    correct the validation error now it will show more precise errors to the user

--- client/src/validation/Schemas/dataSchema.ts
+++ client/src/validation/Schemas/dataSchema.ts
@@ -6,44 +6,38 @@ import type {
 import { enum as enum_, object, string, type ZodType } from "zod";
 
 export const registerSchema: ZodType<UserRegisterTypes> = object({
-  username: string()
-    .min(3, { message: "This field requires at least  3 characters" })
-    .max(20, { message: "Username must not contain more than 10 characters" })
+  username: string().min(1, { message: "username is required" })
+    .min(3, { message: "username must contain at least  3 characters" })
+    .max(20, { message: "username must not contain more than 20 characters" })
     .regex(new RegExp(/^[a-z0-9_.]{1,20}$/), {
       message: "Only underscore_, numbers and lowercase letters are allowed.",
     })
     .toLowerCase(),
-  fullName: string()
-    .min(3, { message: "This field requires at least  3 characters" })
-    .max(50, { message: "Full Name is too long" })
+  fullName: string().min(1, { message: "full Name is required" })
+    .min(3, { message: "full Name must contain at least  3 characters" })
+    .max(50, { message: "full Name is too long" })
     .regex(new RegExp(/^[a-zA-Z0-9]+(?: [a-zA-Z0-9]+)*$/), {
-      message: "Full name is invalid",
+      message: "full name is invalid",
     }),
-  email: string()
-    .min(1, { message: "This field is required" })
-    .max(200, { message: "Email is too long" })
-    .email({ message: "Email is not valid" })
+  email: string().min(1, { message: "email is required" })
+    .min(4, { message: "email must contain atleast 4 characters" })
+    .max(200, { message: "email is too long" })
+    .email({ message: "email is not valid" })
     .toLowerCase(),
-  password: string()
-    .min(6, { message: "This field requires at least 6 characters" })
-    .max(50, { message: "Password is too long" })
+  password: string().min(1, { message: "password is required" })
+    .min(6, { message: "password must contain at least 6 characters" })
     .regex(
       new RegExp(
         /^(?!.*\s)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?/~`-])[A-Za-z0-9!@#$%^&*()_+{}\[\]:;<>,.?/~`-]{6,}$/,
       ),
-      { message: "Password is weak!!" },
-    ),
-  confirmPassword: string()
-    .min(6, { message: "This field requires at least 6 characters" })
-    .max(50, { message: "Password is too long" })
-    .regex(
-      new RegExp(
-        /^(?!.*\s)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?/~`-])[A-Za-z0-9!@#$%^&*()_+{}\[\]:;<>,.?/~`-]{6,}$/,
-      ),
-      { message: "Confirmpassword is invalid" },
-    ),
+      { message: "Password is not strong (e.g: p@ssword)" },
+    )
+    .max(50, { message: "Password is too long" }),
+  confirmPassword: string().min(1, { message: "confirm password  is required" })
+    .min(6, { message: "confirm password must contain at least 6 characters" })
+    .max(50, { message: "Password is too long" }),
 }).refine((data) => data.password === data.confirmPassword, {
-  message: "Confirmpassword is not same as passwords",
+  message: "confirm password is not same as passwords",
   path: ["confirmPassword"],
 });
 
@@ -55,13 +49,13 @@ export const loginSchema: ZodType<UserLoginTypes> = object({
     })
     .min(1, { message: "username or email is required" }),
 
-  password: string().min(1, { message: "This field is required" }),
+  password: string().min(1, { message: "password is required" }),
 });
 
 export const updateSchema: ZodType<UserUpdateTypes> = object({
   username: string()
     .min(3, { message: "This field requires at least  3 characters" })
-    .max(50, { message: "Username must not contain more than 10 characters" })
+    .max(50, { message: "username is too long" })
     .regex(new RegExp(/^[a-z0-9]+(_[a-z0-9]+)?(-[a-z0-9]+)?$/), {
       message: "Username is invalid.",
     })
