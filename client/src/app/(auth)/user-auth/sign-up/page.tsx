72a932e290363c3d6f47e9fb5da49e9a93786fdf
mr-zlaam <zlaam.dev@gmail.com>
2024-10-24 23:54:41 +0500

    add shadow

--- client/src/app/(auth)/user-auth/sign-up/page.tsx
+++ client/src/app/(auth)/user-auth/sign-up/page.tsx
@@ -75,7 +75,7 @@ function RegisterForm() {
       <section className="relative top-20">
         <form onSubmit={handleSubmit(handleRegisterSubmit)} className="">
           <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 ">
-            <div className="border rounded-lg shadow sm:w-full bg-background md:mt-0 sm:max-w-md xl:p-0  border-solid">
+            <div className="border rounded-lg shadow-sm  sm:w-full bg-background md:mt-0 sm:max-w-md xl:p-0  border-solid">
               <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-center text-foreground md:text-2xl">
                   Create an Account
